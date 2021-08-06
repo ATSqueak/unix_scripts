b1e20ed4167fa04a8e09f390693b82487fa3cc85
ATSqueak <arif_taha@yahoo.com>
2021-08-06 15:44:56 +0100

    Update db_login.sh
    Update to only have one connection string and to use a generic set of arguments

--- db_login.sh
+++ db_login.sh
@@ -4,7 +4,7 @@
 #Usage
 echo 'On command line run ./db_login.sh <region> <account_number> <password>'
 echo 'Example region is LDN or TKY or DEV'
-echo 'account_number is just the safe account number'
+echo 'account_number is just the safe account'
 echo 'password is the password copied from Safe page'
  
 if [ $# -ne 3 ]
@@ -14,68 +14,38 @@ then
 fi
  
 #LDN
-db_ldn_user=$2
-db_ldn_pass=$3
- 
-#TKY
-db_tky_user=$2
-db_tky_pass=$3
- 
-#DEV
-db_dev_user=dev_rw
-db_dev_pass=XXX
- 
 if [ $1 == 'LDN' ]
 then
-        #LDN connection string
-        echo '------------------------------------------------------------------'
-        echo 'Database: LDN_DB'
-        echo 'Account user $db_ldn_user'
-        while true; do
-        read -p 'Do you wish to continue? ' yn
-        case $yn in
-          [Yy]* ) break;;
-          [Nn]* ) exit;;
-          * ) echo 'Please answer yes or no.';;
-        esac
-        done
-        ldn_connection_string="sqsh -S LDN_DB -U $db_ldn_user -P $db_ldn_pass"
-        echo 'Running '$ldn_connection_string'...'
-        exec $ldn_connection_string
+    #LDN
+    user=$2
+    pass=$3
+    dbName=$1
 elif [ $1 == 'TKY' ]
 then
-        #TKY connection string
-        echo '------------------------------------------------------------------'
-        echo 'Database: TKY_DB'
-        echo 'Account user $db_tky_user'
-        while true; do
-        read -p 'Do you wish to continue? ' yn
-        case $yn in
-          [Yy]* ) break;;
-          [Nn]* ) exit;;
-          * ) echo 'Please answer yes or no.';;
-        esac
-        done
-        tky_connection_string="sqsh -S TKY_DB -U $db_tky_user -P $db_tky_pass"
-        echo 'Running '$tky_connection_string'...'
-        exec $tky_connection_string
+    #TKY
+    user=$2
+    pass=$3
+    dbName=$1
 elif [ $1 == 'DEV' ]
 then
-       #DEV connection string
-        echo '------------------------------------------------------------------'
-       echo 'Database: DEV_DB'
-        echo 'Account user $db_dev_user'
-        while true; do
-        read -p 'Do you wish to continue? ' yn
-        case $yn in
-          [Yy]* ) break;;
-          [Nn]* ) exit;;
-          * ) echo 'Please answer yes or no.';;
-        esac
-        done
-        dev_connection_string="sqsh -S DEV_DB -U $db_dev_user -P $db_ldn_pass"
-        echo 'Running '$dev_connection_string'...'
-        exec $dev_connection_string
-else
-        echo 'No Region'
+    #DEV
+    user=$2
+    pass=$3
+    dbName=$1
 fi
+
+#LDN connection string
+echo '------------------------------------------------------------------'
+echo "Database: $dbName"
+echo "Account user $user"
+while true; do
+  read -p 'Do you wish to continue? ' yn
+  case $yn in
+  [Yy]* ) break;;
+  [Nn]* ) exit;;
+  * ) echo 'Please answer yes or no.';;
+  esac
+done
+ connection_string="sqsh -S $dbName -U $user -P $pass"
+ echo 'Running '$connection_string'...'
+ exec $connection_string
